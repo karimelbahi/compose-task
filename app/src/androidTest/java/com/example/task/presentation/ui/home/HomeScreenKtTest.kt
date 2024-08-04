fdbab8ebd3bf57e4e29915fb9f55b638f1856735
KarimBahi <kbahi@dollargeneral.com>
2024-08-04 20:06:51 +0300

    enhance ui testing by replacing onNodeWithTag with (onNodeWithTag + childAt) in home screen

--- app/src/androidTest/java/com/example/task/presentation/ui/home/HomeScreenKtTest.kt
+++ app/src/androidTest/java/com/example/task/presentation/ui/home/HomeScreenKtTest.kt
@@ -1,8 +1,11 @@
 package com.example.task.presentation.ui.home
 
+import androidx.compose.ui.test.assert
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertIsNotDisplayed
+import androidx.compose.ui.test.hasTestTag
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onChildAt
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.navigation.compose.rememberNavController
@@ -10,6 +13,7 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.task.common.component.LOADING_DIALOG_TAG
 import com.example.task.presentation.ui.home.HomeScreenTestTags.LAZY_GRID
 import com.example.task.presentation.ui.home.HomeScreenTestTags.LAZY_ROW
+import com.example.task.presentation.ui.home.HomeScreenTestTags.LAZY_ROW_SPACE
 import com.example.task.presentation.ui.home.HomeScreenTestTags.MAIN_COLUMN
 import com.example.task.presentation.ui.home.components.CATEGORY_COMPONENT_LAZY_COLUMN
 import com.example.task.utils.DELAY_6000_SEC
@@ -97,10 +101,18 @@ class HomeScreenTest {
             onNodeWithTag(MAIN_COLUMN).assertIsDisplayed()
             onNodeWithTag(LAZY_ROW).assertIsDisplayed()
             onNodeWithTag(LAZY_GRID).assertIsNotDisplayed()
-            onNodeWithTag(CATEGORY_COMPONENT_LAZY_COLUMN.plus(fakeCategoryList[0].categoryName)).assertIsDisplayed()
-            onNodeWithTag(CATEGORY_COMPONENT_LAZY_COLUMN.plus(fakeCategoryList[1].categoryName)).assertIsDisplayed()
-            onNodeWithTag(CATEGORY_COMPONENT_LAZY_COLUMN.plus(fakeCategoryList[2].categoryName)).assertIsDisplayed()
-
+            onNodeWithTag(LAZY_ROW)
+                .onChildAt(0)
+                .assert(hasTestTag(LAZY_ROW_SPACE.plus(0)))
+            onNodeWithTag(LAZY_ROW)
+                .onChildAt(1)
+                .assert(hasTestTag(CATEGORY_COMPONENT_LAZY_COLUMN.plus(fakeCategoryList[0].categoryName)))
+            onNodeWithTag(LAZY_ROW)
+                .onChildAt(2)
+                .assert(hasTestTag(CATEGORY_COMPONENT_LAZY_COLUMN.plus(fakeCategoryList[1].categoryName)))
+            onNodeWithTag(LAZY_ROW)
+                .onChildAt(3)
+                .assert(hasTestTag(CATEGORY_COMPONENT_LAZY_COLUMN.plus(fakeCategoryList[2].categoryName)))
             runTest {
                 delay(DELAY_6000_SEC)
             }
@@ -109,16 +121,16 @@ class HomeScreenTest {
 
     // TODO:  need to enhance it to use children inside the column
     @Test
-    fun assert_success_state_meals_list_IsShowed(){
-
-        val mockState = MutableStateFlow(
-            HomeState(
-                loading = false,
-                isSuccess = true,
-                categories = fakeCategoryList,
-                meals = fakeMeals
+    fun assert_success_state_meals_list_IsShowed() {
+        val mockState =
+            MutableStateFlow(
+                HomeState(
+                    loading = false,
+                    isSuccess = true,
+                    categories = fakeCategoryList,
+                    meals = fakeMeals,
+                ),
             )
-        )
 
         with(composeTestRule) {
             setContent {
@@ -132,8 +144,6 @@ class HomeScreenTest {
             onNodeWithText(fakeMeals[2].mealName).assertExists()
 
             onNodeWithTag(LOADING_DIALOG_TAG).assertDoesNotExist()
-
         }
     }
-
 }
